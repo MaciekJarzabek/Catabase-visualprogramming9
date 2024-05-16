89620e1d919f6e351f0c1196525c13bf6304a78c
Maciej Jarząbek <mmaciej.jarzabekk@outlook.com>
2024-05-16 23:04:58 +0200

    Dodaj pliki projektów.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,454 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            textBox12 = new TextBox();
+            textBox13 = new TextBox();
+            textBox14 = new TextBox();
+            textBox15 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            label17 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            listBox1 = new ListBox();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(233, 24);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(555, 25);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(233, 85);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(204, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(233, 145);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(233, 209);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(204, 27);
+            textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(233, 279);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(204, 27);
+            textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(233, 345);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(125, 27);
+            textBox7.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(233, 418);
+            textBox8.Multiline = true;
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(125, 30);
+            textBox8.TabIndex = 7;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(233, 472);
+            textBox9.Multiline = true;
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(447, 65);
+            textBox9.TabIndex = 8;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(555, 563);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(125, 27);
+            textBox10.TabIndex = 9;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(1094, 103);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(230, 27);
+            textBox11.TabIndex = 10;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(1094, 136);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(230, 27);
+            textBox12.TabIndex = 11;
+            // 
+            // textBox13
+            // 
+            textBox13.Location = new Point(1094, 169);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(230, 27);
+            textBox13.TabIndex = 12;
+            // 
+            // textBox14
+            // 
+            textBox14.Location = new Point(876, 252);
+            textBox14.Name = "textBox14";
+            textBox14.Size = new Size(125, 27);
+            textBox14.TabIndex = 13;
+            // 
+            // textBox15
+            // 
+            textBox15.Location = new Point(1284, 252);
+            textBox15.Name = "textBox15";
+            textBox15.Size = new Size(125, 27);
+            textBox15.TabIndex = 14;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(119, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 20);
+            label1.TabIndex = 15;
+            label1.Text = "numer albumu:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(507, 28);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 20);
+            label2.TabIndex = 16;
+            label2.Text = "data:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(119, 88);
+            label3.Name = "label3";
+            label3.Size = new Size(113, 20);
+            label3.TabIndex = 17;
+            label3.Text = "nazwisko i imię:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(141, 148);
+            label4.Name = "label4";
+            label4.Size = new Size(63, 20);
+            label4.TabIndex = 18;
+            label4.Text = "semestr:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(51, 212);
+            label5.Name = "label5";
+            label5.Size = new Size(184, 20);
+            label5.TabIndex = 19;
+            label5.Text = "kierunek i stopień studiów:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(92, 282);
+            label6.Name = "label6";
+            label6.Size = new Size(135, 20);
+            label6.TabIndex = 20;
+            label6.Text = "nazwa przedmiotu:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(119, 348);
+            label7.Name = "label7";
+            label7.Size = new Size(112, 20);
+            label7.TabIndex = 21;
+            label7.Text = "liczba punktów:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(11, 421);
+            label8.Name = "label8";
+            label8.Size = new Size(216, 20);
+            label8.TabIndex = 22;
+            label8.Text = "imię i nazwisko prowadzącego:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(127, 475);
+            label9.Name = "label9";
+            label9.Size = new Size(100, 20);
+            label9.TabIndex = 23;
+            label9.Text = "Uzasadnienie:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 15F);
+            label10.Location = new Point(1034, 17);
+            label10.Name = "label10";
+            label10.Size = new Size(112, 35);
+            label10.TabIndex = 24;
+            label10.Text = "DEZYCJA";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(491, 566);
+            label11.Name = "label11";
+            label11.Size = new Size(58, 20);
+            label11.TabIndex = 25;
+            label11.Text = "podpis:";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(828, 103);
+            label12.Name = "label12";
+            label12.Size = new Size(230, 20);
+            label12.TabIndex = 26;
+            label12.Text = "imię i nazwisko członków komisji:";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(1068, 106);
+            label13.Name = "label13";
+            label13.Size = new Size(20, 20);
+            label13.TabIndex = 27;
+            label13.Text = "1.";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(1068, 139);
+            label14.Name = "label14";
+            label14.Size = new Size(20, 20);
+            label14.TabIndex = 28;
+            label14.Text = "2.";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(1068, 172);
+            label15.Name = "label15";
+            label15.Size = new Size(20, 20);
+            label15.TabIndex = 29;
+            label15.Text = "3.";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(816, 255);
+            label16.Name = "label16";
+            label16.Size = new Size(42, 20);
+            label16.TabIndex = 30;
+            label16.Text = "data:";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Location = new Point(1220, 255);
+            label17.Name = "label17";
+            label17.Size = new Size(58, 20);
+            label17.TabIndex = 31;
+            label17.Text = "podpis:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(942, 358);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 32;
+            button1.Text = "Zapisz";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(1068, 358);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 33;
+            button2.Text = "Odczytaj";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(807, 404);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(639, 204);
+            listBox1.TabIndex = 35;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(1202, 358);
+            button3.Name = "button3";
+            button3.Size = new Size(207, 29);
+            button3.TabIndex = 36;
+            button3.Text = "Usuń wastości z bazy danych";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1480, 622);
+            Controls.Add(button3);
+            Controls.Add(listBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label17);
+            Controls.Add(label16);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox15);
+            Controls.Add(textBox14);
+            Controls.Add(textBox13);
+            Controls.Add(textBox12);
+            Controls.Add(textBox11);
+            Controls.Add(textBox10);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox11;
+        private TextBox textBox12;
+        private TextBox textBox13;
+        private TextBox textBox14;
+        private TextBox textBox15;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private Label label17;
+        private Button button1;
+        private Button button2;
+        private ListBox listBox1;
+        private Button button3;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,226 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private readonly string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\mjarz\source\repos\WinFormsApp1\WinFormsApp1\Database1.mdf"";Integrated Security=True";
+        private readonly DatabaseManager dbManager;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dbManager = new DatabaseManager(connectionString);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInputs()) return;
+            var user = GetUserFromInputs();
+            dbManager.WriteData(user);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var users = dbManager.ReadData();
+            UpdateListBox(users);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Czy na pewno chcesz wyczyœciæ wszystkie dane z bazy?", "Potwierdzenie", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                dbManager.ClearData();
+                MessageBox.Show("Baza danych zosta³a wyczyszczona.");
+            }
+        }
+
+        private bool ValidateInputs()
+        {
+            foreach (Control control in Controls)
+            {
+                if (control is TextBox textBox && string.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    MessageBox.Show($"{textBox.Tag} nie mo¿e byæ puste.");
+                    return false;
+                }
+            }
+            if (!DateTime.TryParseExact(textBox2.Text, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out _) || !DateTime.TryParseExact(textBox14.Text, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out _))
+            {
+                MessageBox.Show("Data musi byæ w formacie DD.MM.YYYY.");
+                return false;
+            }
+            if (!int.TryParse(textBox7.Text, out _))
+            {
+                MessageBox.Show("Punkty musz¹ byæ liczb¹ ca³kowit¹.");
+                return false;
+            }
+            return true;
+        }
+
+        private User GetUserFromInputs()
+        {
+            return new User
+            {
+                nr_albumu = textBox1.Text,
+                data1 = DateTime.ParseExact(textBox2.Text, "dd.MM.yyyy", null),
+                imie = textBox3.Text,
+                sem = textBox4.Text,
+                kierunek = textBox5.Text,
+                przedmiot = textBox6.Text,
+                punkty = int.Parse(textBox7.Text),
+                prowadzacy = textBox8.Text,
+                uzasadnienie = textBox9.Text,
+                podpis1 = textBox10.Text,
+                komisja1 = textBox11.Text,
+                komisja2 = textBox12.Text,
+                komisja3 = textBox13.Text,
+                data2 = DateTime.ParseExact(textBox14.Text, "dd.MM.yyyy", null),
+                podpis2 = textBox15.Text
+            };
+        }
+
+        private void UpdateListBox(List<User> users)
+        {
+            listBox1.Items.Clear();
+            if (users.Count == 0)
+            {
+                MessageBox.Show("Nie znaleziono ¿adnych danych do odczytu.");
+                return;
+            }
+            foreach (var user in users)
+            {
+                listBox1.Items.Add("Nr albumu: " + user.nr_albumu);
+                listBox1.Items.Add("Data 1: " + user.data1.ToString("dd.MM.yyyy"));
+                listBox1.Items.Add("Imiê: " + user.imie);
+                listBox1.Items.Add("Semestr: " + user.sem);
+                listBox1.Items.Add("Kierunek: " + user.kierunek);
+                listBox1.Items.Add("Przedmiot: " + user.przedmiot);
+                listBox1.Items.Add("Punkty: " + user.punkty);
+                listBox1.Items.Add("Prowadz¹cy: " + user.prowadzacy);
+                listBox1.Items.Add("Uzasadnienie: " + user.uzasadnienie);
+                listBox1.Items.Add("Podpis 1: " + user.podpis1);
+                listBox1.Items.Add("Komisja 1: " + user.komisja1);
+                listBox1.Items.Add("Komisja 2: " + user.komisja2);
+                listBox1.Items.Add("Komisja 3: " + user.komisja3);
+                listBox1.Items.Add("Data 2: " + user.data2.ToString("dd.MM.yyyy"));
+                listBox1.Items.Add("Podpis 2: " + user.podpis2);
+                listBox1.Items.Add("*************************************");
+                listBox1.Items.Add(""); // Dodaj pust¹ liniê po ka¿dym u¿ytkowniku
+            }
+        }
+    }
+
+    public class DatabaseManager
+    {
+        private readonly string connectionString;
+
+        public DatabaseManager(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<User> ReadData()
+        {
+            var userList = new List<User>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand("SELECT * FROM [dbo].[Table]", connection))
+            {
+                try
+                {
+                    connection.Open();
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var user = new User();
+                        for (var i = 0; i < reader.FieldCount; i++)
+                        {
+                            var propName = reader.GetName(i);
+                            var propValue = reader.GetValue(i);
+                            typeof(User).GetProperty(propName)?.SetValue(user, propValue);
+                        }
+                        userList.Add(user);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error reading data: " + ex.Message);
+                }
+            }
+            return userList;
+        }
+
+        public void WriteData(User user)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand("INSERT INTO [dbo].[Table] VALUES (@nr_albumu, @data1, @imie, @sem, @kierunek, @przedmiot, @punkty, @prowadzacy, @uzasadnienie, @podpis1, @komisja1, @komisja2, @komisja3, @data2, @podpis2)", connection))
+            {
+                command.Parameters.AddWithValue("@nr_albumu", user.nr_albumu);
+                command.Parameters.AddWithValue("@data1", user.data1);
+                command.Parameters.AddWithValue("@imie", user.imie);
+                command.Parameters.AddWithValue("@sem", user.sem);
+                command.Parameters.AddWithValue("@kierunek", user.kierunek);
+                command.Parameters.AddWithValue("@przedmiot", user.przedmiot);
+                command.Parameters.AddWithValue("@punkty", user.punkty);
+                command.Parameters.AddWithValue("@prowadzacy", user.prowadzacy);
+                command.Parameters.AddWithValue("@uzasadnienie", user.uzasadnienie);
+                command.Parameters.AddWithValue("@podpis1", user.podpis1);
+                command.Parameters.AddWithValue("@komisja1", user.komisja1);
+                command.Parameters.AddWithValue("@komisja2", user.komisja2);
+                command.Parameters.AddWithValue("@komisja3", user.komisja3);
+                command.Parameters.AddWithValue("@data2", user.data2);
+                command.Parameters.AddWithValue("@podpis2", user.podpis2);
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Dane zapisane pomyœlnie.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error writing data " + ex.Message);
+                }
+            }
+        }
+
+        public void ClearData()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand("DELETE FROM [dbo].[Table]", connection))
+            {
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error clearing data: " + ex.Message);
+                }
+            }
+        }
+    }
+
+    public class User
+    {
+        public string nr_albumu { get; set; }
+        public DateTime data1 { get; set; }
+        public string imie { get; set; }
+        public string sem { get; set; }
+        public string kierunek { get; set; }
+        public string przedmiot { get; set; }
+        public int punkty { get; set; }
+        public string prowadzacy { get; set; }
+        public string uzasadnienie { get; set; }
+        public string podpis1 { get; set; }
+        public string komisja1 { get; set; }
+        public string komisja2 { get; set; }
+        public string komisja3 { get; set; }
+        public DateTime data2 { get; set; }
+        public string podpis2 { get; set; }
+    }
+}
